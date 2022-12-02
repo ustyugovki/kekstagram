2187ac196c6d3778fca5231d9fab21581c40aa58
kos <ustyugovki@bk.ru>
2022-12-02 14:34:01 +0300

    Сделал функцию проверки длины комментария

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const MIN_INTEGER = 1;
+const MAX_INTEGER = 25;
+const commentLength = 40;
+
 /**
  * @param {number} min
  * @param {number} max
@@ -10,5 +14,21 @@ const getRandomInt = (min, max) => {
   return Math.round((max - min) * Math.random() + min);
 };
 
-getRandomInt(1, 25);
-// console.log(getRandomInt(1, 25));
+getRandomInt(MIN_INTEGER, MAX_INTEGER);
+// console.log(getRandomInt(MIN_INTEGER, MAX_INTEGER));
+
+
+/**
+ * @param {string} value
+ * @param {number} valueParam
+ */
+const checkLengthString = (value, valueParam) => {
+  if (value.length > valueParam) {
+    throw new Error(`Комментарий превышает ${valueParam} символов!!!`);
+  }
+
+  return value;
+};
+
+checkLengthString('Проверка длины комментария', commentLength);
+// console.log(checkLengthString('Проверка длины комментария', commentLength));

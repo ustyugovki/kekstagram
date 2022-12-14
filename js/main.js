f0caa055bea7ee187b6b427b20712f56a3a84e8e
kos <ustyugovki@bk.ru>
2022-12-14 15:23:22 +0300

    Подправил функции. Добавил типизацию

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const commentLength = 40;
 
 
 /**
+ * Генерирует рандомное число
  * @param {number} min
  * @param {number} max
  */
@@ -12,27 +13,46 @@ const getRandomInt = (min, max) => {
   if (!Number.isInteger(min) || min < 0 || !Number.isInteger(max) || max < 0 || max < min) {
     return NaN;
   }
-
+  // почему при round появляется undefined ???
   return Math.floor(Math.random() * (max - min) + min);
 };
 
 
-const getClearArray = (minIn, maxIn) => {
-  for (let i = minIn; i <= maxIn; ++i) {
+/**
+ * Генерирует массив определенного диапазона
+ * @param {number} min
+ * @param {number} max
+ * @returns {number[]}
+ */
+const getClearArray = (min, max) => {
+  for (let i = min; i <= max; ++i) {
     clearArray[i - 1] = i;
   }
-
   return clearArray;
 };
+// console.log(getClearArray(MIN, MAX));
 
 
+/**
+ * Генерирует неповторяющийся рандомный массив из диапазона
+ * @param {number[]} array
+ */
 const getRandomArray = (array) => {
   let currentIndex = array.length - 1;
-  let randomIndex;
+  let randomIndex = 0;
+  let temp = 0;
 
-  for (currentIndex; currentIndex > 0; currentIndex--) {
-    randomIndex = Math.floor(getRandomInt(MIN, MAX));
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  for (currentIndex; currentIndex >= 0; currentIndex--) {
+    randomIndex = getRandomInt(MIN, MAX);
+
+    // Here's a JavaScript implementation of the Durstenfeld shuffle,
+    // an optimized version of Fisher-Yates:
+    temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
+
+    // for ES6/ECMAScript 2015
+    // [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
   }
 
   return array;
